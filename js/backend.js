40691de6b7a1309b3b201142e5077273e2e03264
romanver777 <33807882+romanver777@users.noreply.github.com>
2021-11-22 14:27:56 +0300

    Update backend.js

--- js/backend.js
+++ js/backend.js
@@ -1,7 +1,7 @@
 // получение и отправка данных на сервер
 (function () {
 
-	const url     = 'https://run.mocky.io/v3/2dc10ffc-6caa-4204-8462-c627aafbfec9';
+	const url     = 'https://run.mocky.io/v3/02446568-e12a-4311-b8e0-55f83db67c45';
 	const urlSend= 'https://javascript.pages.academy/keksobooking';
 
     window.load = (onSuccess, onError) => {
